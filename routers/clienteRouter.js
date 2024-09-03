65427f7b34c7cde652cbe28251b78849c58cb41a
Jorge Moreno <jmorenorosania@gmail.com>
2024-09-02 23:04:42 -0500

    enable put method

--- routers/clienteRouter.js
+++ routers/clienteRouter.js
@@ -5,8 +5,8 @@ const clienteController = require('../controller/clienteController');
 router.post('/', clienteController.agregarClientes);
 router.get('/', clienteController.buscarClientes);
 router.get('/:id', clienteController.buscarClientesId);
-// router.put('/:id', clienteController.modificarClientes);
-router.patch('/:id', clienteController.editarClientes);
+router.put('/:id', clienteController.modificarClientes);
+// router.patch('/:id', clienteController.editarClientes);
 router.delete('/:id', clienteController.eliminarClientesId);
 
 module.exports = router;
